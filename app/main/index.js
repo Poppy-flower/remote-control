bbb6bdd5a333ce54996d86ae1aec4642c08160dc
chenhong <chenhong03@baijiahulian.com>
2020-09-12 15:32:35 +0800

    commit 搭建项目

---
+++ app/main/index.js
@@ -0,0 +1,22 @@
+const {app, BrowserWindow} = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
+
+let win
+
+app.on('ready', () => {
+    win = new BrowserWindow({
+        width: 600,
+        height: 300,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    if (isDev) {
+        win.loadURL('http://localhost:3000')
+    } else {
+        //第三章会用到
+        win.loadURL(path.resolve(__dirname, '../renderer/pages/main/index.html'))
+    }
+})
